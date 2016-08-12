d9ce0e92e4f1b4db9d92f7630909862b0e52364d
Peter <peter.helsen@hi3.be>
2016-08-12 21:17:54 +0200

    smaller interface : encrypt/decrypt/selftest

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject tectus "0.1.0"
-  :description "Very simple encryption of strings"
-  :url  "https://github.com/phelsen/tectus"
+(defproject tectus "0.1.3"
+  :description "Simple string encryption"
+  :url  "https://github.cnom/phelsen/tectus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
--- src/tectus/core.clj
+++ src/tectus/core.clj
@@ -1,39 +1,36 @@
 (ns tectus.core)
 
-
-(ns gludox.cryptify)
-
-(defn str->nb [str_]
+(defn- str->nb [str_]
   (apply + (map int str_)))
 
-(defn distribute [nb nb-parts]
+(defn-  distribute [nb nb-parts]
   (for [i  (range 1 (inc nb-parts))]
     (* i (int (/ nb nb-parts)))))
 
-(defn  shorten  [str_ nb]
+(defn-  shorten  [str_ nb]
   (let [sum (str->nb str_)
         dist (distribute sum nb)]
     (apply str (map char dist))))
 
-(defn my+ [a b]
+(defn- my+ [a b]
   (let [sum (+ a b)]
     (if (> sum 65535) (- sum 65535) sum)))
 
-(defn my- [a b]
+(defn- my- [a b]
   (let [sum (- a b)]
     (if (< sum 0) (+ sum 65535) sum)))
 
 
-(defn nth_ [ str_ n]
+(defn- nth_ [ str_ n]
   (let [l (count str_)
         n (if (>= n l) (mod n l) n)]
     (nth str_  n)))
 
-(defn trans [encrypt? elem nb key_]
+(defn- trans [encrypt? elem nb key_]
   (let [step (-> key_ (nth_ nb) int)]
     (if encrypt? (my+ elem step) (my- elem step))))
 
-(defn en-or-de-crypt  [str_ key_ encrypt?]
+(defn- en-or-de-crypt  [str_ key_ encrypt?]
   (let [str-len (count str_)
         key-len (count key_)
         key__ (if (< str-len key-len) (shorten key_ str-len) key_)
@@ -42,6 +39,6 @@
         chars_ (map char transed)]
     (apply str chars_)))
 
-(defn encr [str_ key_] (en-or-de-crypt str_ key_ true ))
-(defn decr [str_ key_] (en-or-de-crypt str_ key_ false ))
-
+(defn encrypt [str_ key_] (en-or-de-crypt str_ key_ true ))
+(defn decrypt  [str_ key_] (en-or-de-crypt str_ key_ false ))
+(defn self-test [] ( = (-> "tectus" (encrypt "jos") (decrypt "jos")) "tectus"))
