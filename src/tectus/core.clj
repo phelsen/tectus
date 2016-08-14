4cf33912fdc091aec9b9477579d4747144cdbd73
Peter <peter.helsen@hi3.be>
2016-08-14 18:04:12 +0200

    Adding support for binary files

--- src/tectus/core.clj
+++ src/tectus/core.clj
@@ -1,4 +1,5 @@
-(ns tectus.core)
+(ns tectus.core
+  (require [clojure.java.io :as io]))
 
 (defn- str->nb [str_]
   (apply + (map int str_)))
@@ -20,7 +21,6 @@
   (let [sum (- a b)]
     (if (< sum 0) (+ sum 65535) sum)))
 
-
 (defn- nth_ [ str_ n]
   (let [l (count str_)
         n (if (>= n l) (mod n l) n)]
@@ -39,6 +39,55 @@
         chars_ (map char transed)]
     (apply str chars_)))
 
+
 (defn encrypt [str_ key_] (en-or-de-crypt str_ key_ true ))
 (defn decrypt  [str_ key_] (en-or-de-crypt str_ key_ false ))
+
+
+(defn- file-path->byte-array [url]
+  (let [f (io/file url)
+        ar (byte-array (.length f))
+        is (io/input-stream f)
+        ]
+    (.read is ar)
+    (.close is)
+    ar))
+
+(defn- byte-array->file-path [arr url]
+  (with-open [os (io/output-stream url)]
+    (.write  os arr)))
+
+
+(defn- bytes->byte-string [bytes_]
+  (apply str
+         (for [b bytes_]
+           (char  (+ 255 b  )))))
+
+(defn-  byte-string->bytes
+"only for 'byte-strings', strings under 256"
+  [str_]
+  (byte-array
+   (for [i str_]
+     (-  (int i) 255))))
+
+(defn- bytes->encrypted-byte-string [b key_]
+  (encrypt  
+   (bytes->byte-string b)
+   key_))
+
+(defn- encrypted-byte-string->bytes [bs key_]
+  (byte-string->bytes (decrypt bs key_)))
+
 (defn self-test [] ( = (-> "tectus" (encrypt "jos") (decrypt "jos")) "tectus"))
+
+
+(defn encrypt-file-to [file-path encrypted-file-path key_]
+    (let [bytes_ (file-path->byte-array file-path)
+          encr-bs (bytes->encrypted-byte-string bytes_ key_)]
+      (spit  encrypted-file-path encr-bs)))
+
+(defn decrypt-file-to [encrypted-file-path decrypted-file-path key_]
+  (let [encr-bs (slurp encrypted-file-path)        
+        decr-bytes (encrypted-byte-string->bytes encr-bs key_)]
+      (byte-array->file-path decr-bytes decrypted-file-path)))
+
